8e644932fe74bd4a7ac468b47ecd96ead276bf00
enricop <enrico.papi@gmail.com>
2016-10-13 09:28:37 +0200

    project 4 reviewed

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -20,9 +20,8 @@ class LearningAgent(Agent):
         self.discount_factor = gamma # discount
         
         #Final analysis variables
-        self.trip_rewards = [] #log of all trips in an list [(penalty, reward),]        
+        self.trips_rewards = [] #log of all trips rewards in a list
         self.passed_trips = 0  #number of passed trips
-        self.failed_trips = 0  #number of failed trips
 
     def reset(self, destination=None):
         '''called before each trip'''
@@ -33,10 +32,10 @@ class LearningAgent(Agent):
         self.trip_reward = 0  #points gained for a single trip
 
     def greedy_action(self, state):
-        '''returns the action with the best known q value for a given state.'''
+        '''returns the action with the best known Q value for a given state.'''
         Qvals = [self.Q.get((state, action), 0.0) for action in self.directions]
         max_Qval = max(Qvals)
-        if Qvals.count(max_Qval) > 1: #mult existing values
+        if Qvals.count(max_Qval) > 1: #multiple existing states
             best = [i for i in range(len(self.directions)) if Qvals[i] == max_Qval] #create list of indexes
             direction_idx = random.choice(best) 
         else:
@@ -44,35 +43,41 @@ class LearningAgent(Agent):
         return self.directions[direction_idx]
 
     def choose_action(self, state):
-        '''decide if we should take a random action or a greedy action.
-           This exploration_rate value decreases for each of the completed trips
-           It is used for initial choice between greedy and random moves
-           The initial rate goes down very quickly so greedy actions are chosen after some initial moves'''
-        if random.random() < (float(self.exploration_rate) / (len(self.trip_rewards) + float(self.exploration_rate))): # Compare epsilon with random 0..1 value
+        '''Decide if we should take a random action or a greedy action.
+           
+           This random_factor value decreases for each of the completed trips.
+           It is used for initial choice between greedy and random moves.
+           
+           The initial random_factor decreases as trips get completed
+           so greedy actions are chosen after some initial moves
+           It depends on the eploration_rate value'''
+        random_factor = self.exploration_rate / (self.passed_trips + self.exploration_rate)
+
+        if random.random() < random_factor: # Compare it with random 0..1 values
             action = random.choice(self.directions) # Random action for exploring the enviroment
         else:
             action =  self.greedy_action(state) #Pick the best value for Q - Greedy action
         return action
 
     def learnQ(self, prev_state, action, reward, next_state):
-        '''determine q val and update lookup.'''
-        if prev_state is None:
-            return
-            
-        prev_val = self.Q.get((prev_state, action), None)
+        '''Determine Q val and update Q table.sss
+           From here we can see that we have a visibility on one state ahead,
+           and the learning is based only on the current state and the Q table'''
+
+        # For unexplored sets of states we get 0 Q value.
+        # The learning rate and current reward will determine the final Q value
+        prev_val = self.Q.get((prev_state, action), 0.0)
+        next_val = max([self.Q.get((next_state, action), 0.0) for direction in self.directions])
         
-        if prev_val is None:
-            next_val = reward
-        else:
-            next_val = max([self.Q.get((next_state, action), 0.0) for direction in self.directions])
-            #generates a discounted Q value to slightly update Q.
-            learned_val =  reward + self.discount_factor * next_val
-            
-            #         last q val + ( gamma          * utility of the next state)
-            next_val = prev_val + (self.learning_rate * (learned_val - prev_val))
+        # Generates a discounted Q value to slightly update Q.
+        learned_val =  reward + self.discount_factor * next_val
+        # Even with unexplored states we multiply the learning rate with the reward
         
-        # set the new qval for the previous state
-        self.Q[(prev_state, action)] = next_val
+        #         last Q val + ( gamma           * utility of the next state)
+        new_val = prev_val + (self.learning_rate * (learned_val - prev_val))
+        
+        # set the new Qval for the previous state
+        self.Q[(prev_state, action)] = new_val
 
     def update(self, t):
         '''main loop called at the begining of each state'''
@@ -91,38 +96,26 @@ class LearningAgent(Agent):
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        #get the new state after taking the action
+        # Get the new state and waypoint after taking the action
         inputs = self.env.sense(self)
+        self.next_waypoint = self.planner.next_waypoint()
         
         new_state = (self.next_waypoint, inputs['light'], inputs['oncoming'], inputs['left'])
 
         # Learn policy based on state, action, reward, new_state
         self.learnQ(self.state, action, reward, new_state)
 
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)
 
         '''Logging for analysis of the agent.'''
-        #if the reward is negative we consider it a penalty
-        if reward < 0: 
-            self.trip_penalty += reward
-        else:
-            self.trip_reward += reward
+        self.trip_reward += reward
 
-        #determine if the agent has reached the destination within the number of alloted steps.'''
+        # Determine if the agent has reached the destination within the number of alloted steps.
         if(self.env.done == True):
             self.passed_trips += 1
-            self.trip_rewards.append((self.trip_penalty, self.trip_reward))
-            
+            self.trips_rewards.append(self.trip_reward)
         elif(self.env.get_deadline(self) <= 0):
-            self.failed_trips += 1
-            self.trip_rewards.append((self.trip_penalty, self.trip_reward))
- 
-def mean_reward(two_d_array):
-    '''Calculate the mean reward by adding together the rewards and the penalties'''
-    reward_tot = 0
-    for reward in two_d_array:
-        reward_tot += reward[0]+reward[1]
-    return reward_tot/len(two_d_array) 
+            self.trips_rewards.append(self.trip_reward)
 
 def run():
     """Run the agent for a finite number of trips."""
@@ -130,7 +123,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to tracks
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trips
 
     # Now simulate it
@@ -140,11 +133,11 @@ def run():
     sim.run(n_trials=100)  # run for a specified number of trips
     # NOTE: To quit midway, press Esc or close pygame window, or hit Ctrl+C on the command-line
 
-    pass_rate = float(a.passed_trips)/ float(len(a.trip_rewards)) * 100
+    pass_rate = a.passed_trips * 100.0 / len(a.trips_rewards)
 
     #Print stats after running a finite number of trips.
     print("Learning Rate: {}, Discount Factor: {}, exploration_rate {}, Pass Rate: {}%, Explored States: {}, Mean Reward: {}, Number of Trials: {}").format(
-        a.learning_rate, a.discount_factor, a.exploration_rate, pass_rate, len(a.Q), mean_reward(a.trip_rewards), len(a.trip_rewards))
+        a.learning_rate, a.discount_factor, a.exploration_rate, pass_rate, len(a.Q), sum(a.trips_rewards)/len(a.trips_rewards), len(a.trips_rewards))
 
 if __name__ == '__main__':
     run()
